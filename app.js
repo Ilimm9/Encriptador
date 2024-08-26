8cba6681a1621dbf479d421798114bb1f0d4ddb8
Ilian Martínez <ili03maro@gmail.com>
2024-08-26 02:29:16 -0600

    Realizando conexion js con html y simplicando codigo

--- app.js
+++ app.js
@@ -1,34 +1,42 @@
+// funciones para obtener y devolver texto
+function ObtenerTxt (){
+     const texto = document.getElementById('mensaje').value;
+    return texto ? texto : "";  // Asegurar que devuelva una cadena vacía si es indefinido o nulo
+ }
 
-// funciones para encriptar y desencriptar
-function Encriptar(texto){
-    let textoEncriptado = texto.replace(/a|e|i|o|u/g, function(match) {
-      if (match === 'a') {
-          return 'ai';
-      } else if (match === 'e') {
-          return 'enter';
-      }else if (match === 'i') {
-          return 'imes';
-      }else if (match === 'o') {
-          return 'ober';
-      }else if (match === 'u') {
-          return 'ufat';
-      }
-  }); return textoEncriptado;
-  }
+ // funciones para encriptar y desencriptar
+ //vincular con los campos de texto del html
 
-    
-  function Desencriptar(texto){
-    let textoDescifrado = texto.replace(/ai|enter|imes|ober|ufat/g, function(match) {
-      if (match === 'ai') {
-          return 'a';
-      } else if (match === 'enter') {
-          return 'e';
-      }else if (match === 'imes') {
-          return 'i';
-      }else if (match === 'ober') {
-          return 'o';
-      }else if (match === 'ufat') {
-          return 'u';
-      }
-  }); return textoDescifrado;
-  }
+ function BtnEncriptar() {
+    const texto = ObtenerTxt();
+    if (texto) {
+        let textoEncriptado = texto.replaceAll(/a|e|i|o|u/g, function(match) {
+            switch (match) {
+                case 'a': return 'ai';
+                case 'e': return 'enter';
+                case 'i': return 'imes';
+                case 'o': return 'ober';
+                case 'u': return 'ufat';
+            }
+      });
+        return document.getElementById('resultado').value = textoEncriptado;
+    } else {
+        alert("Por favor ingrese un texto para encriptar.");
+    }
+}
+
+function BtnDesencriptar() {
+    const texto = ObtenerTxt(); 
+    if (texto) { let textoDescifrado = texto.replaceAll(/ai|enter|imes|ober|ufat/g, function(match) {
+        switch (match) {
+            case 'ai': return 'a';
+            case 'enter': return 'e';
+            case 'imes': return 'i';
+            case 'ober': return 'o';
+            case 'ufat': return 'u';
+        }
+  }); return document.getElementById('resultado').value = textoDescifrado;
+    } else {
+        alert("Por favor ingrese un texto para desencriptar.");
+    }
+}
